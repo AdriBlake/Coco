304720e328f8fb01d6836b50f56d9dd47f705fc4
AdriBlake <45843259+AdriBlake@users.noreply.github.com>
2018-12-20 12:33:30 +0100

    L1_v001

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public GameObject Miguel;
+
+    private Miguel pn;
+
+    // Use this for initialization
+    void Start()
+    {
+        pn = Miguel.GetComponent<Miguel>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posx = pn.transform.position.x;
+        transform.position = new Vector3(posx, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Miguel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miguel : MonoBehaviour {
+
+        public int LifeNumb;
+        public float force;
+        public float velocity;
+        private Animator animator;
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            LifeNumb = 7;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.D))
+            { // Si el jugador toca la tecla D
+                animator.SetInteger("state", 1);
+                GetComponent<SpriteRenderer>().flipX = false;
+                transform.Translate(velocity * Time.deltaTime, 0.0f, 0.0f);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            { // Si el jugador toca la tecla D
+                animator.SetInteger("state", 1);
+                GetComponent<SpriteRenderer>().flipX = true;
+                transform.Translate(-velocity * Time.deltaTime, 0.0f, 0.0f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetTrigger("Jump");
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, force));
+            }
+
+            if (!Input.anyKey)
+            {
+                animator.SetInteger("state", 0);
+            }
+        }
+    }
